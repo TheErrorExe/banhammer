9f84c1e318418c471f0f0c16f114c87b2d39982d
TheErrorExe <161362055+TheErrorExe@users.noreply.github.com>
2025-03-13 21:37:09 +0100

    Update banhammer.py

--- banhammer.py
+++ banhammer.py
@@ -1,5 +1,21 @@
 print("Banhammer Discord Bot, Copyright (C) 2025 TheErrorExe. This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are welcome to redistribute it under certain conditions. This Bot is licensed under the GPLv3 (GNU General Public License). You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.")
 
+"""
+Copyright (C) 2025 TheErrorExe
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import discord
 from discord.ext import commands, tasks
 import yaml
